16f327ce5a23a649c1e30959b4d6f92a56ba594f
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2021-05-16 19:12:36 -0300

    Cleanup

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module Applogger
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.hosts << "brave-moose-19.loca.lt"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
